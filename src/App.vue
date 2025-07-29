29c2712668d67793fa177f3203216acb57b2c94e
Mohamed El aassal <142220085+MohamedElaassal@users.noreply.github.com>
2025-07-29 19:14:16 +0100

    components and Props

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
+</script>
+
+<template>
+  <Navbar/>
+  <Hero title="test title" subtitle="change subtitle"/>
+  <HomeCards/>
+  
+  
+</template>
+
